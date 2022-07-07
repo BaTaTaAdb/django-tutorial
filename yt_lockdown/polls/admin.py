5bfa44f0db6b3ef841ee5c21e91ec74bf8053252
Joao Nuno <joaominecraft.adr@gmail.com>
2022-07-07 21:21:23 +0100

    change default admin django config and classtype

--- yt_lockdown/polls/admin.py
+++ yt_lockdown/polls/admin.py
@@ -3,15 +3,18 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
-    extra = 3
+    extra = 1
 
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [("Date infomation", {"fields": ['question_text']}),
                  (None, {"fields": ['pub_date'], "classes": ["collapse"]})]
     inlines = [ChoiceInline]
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
 
 
 admin.site.register(Question, QuestionAdmin)
--- yt_lockdown/yt_lockdown/settings.py
+++ yt_lockdown/yt_lockdown/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'yt_lockdown.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
